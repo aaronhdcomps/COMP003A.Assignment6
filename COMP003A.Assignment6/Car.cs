9789fd1f531d919410beec8bea58682ac65f4a94
aaronhdcomps <aaron.hdcomps@gmail.com>
2023-02-24 21:05:26 -0800

    Finished Cars class

--- COMP003A.Assignment6/Car.cs
+++ COMP003A.Assignment6/Car.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace COMP003A.Assignment6
 {
-    internal class Car
+    /// <summary>
+    /// Car extends the Vehicle class
+    /// </summary>
+    internal class Car : Vehicle
     {
+        /***** constructors section *****/
+        // by default, a class has a default constructor that does not take any parameters
+        // therefore, you do not have to explicitly create one
+        public override void GetInfo()
+        {
+            Console.WriteLine($"I'm a {nameof{Car}}");
+            Console.WriteLine($"I don't want to disclose my information");
+        }
     }
 }
